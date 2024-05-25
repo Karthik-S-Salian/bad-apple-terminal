15458dba4812436c6ab16d3ce59f0d3ae9a8c8d4
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-05-25 19:43:26 +0530

    moved downsampling to ffmpeg

--- src/main.rs
+++ src/main.rs
@@ -41,15 +41,26 @@ fn main() -> Result<(), ffmpeg::Error> {
 
         //:TODO
         //downscale frame in ffmpeg itself
-        let mut scaler = Context::get(
-            decoder.format(),
-            decoder.width(),
-            decoder.height(),
-            Pixel::GRAY8,
-            decoder.width(),
-            decoder.height(),
-            Flags::AREA,
-        )?;
+        // let mut scaler = Context::get(
+        //     decoder.format(),
+        //     decoder.width(),
+        //     decoder.height(),
+        //     Pixel::YA8,
+        //     decoder.width(),
+        //     decoder.height(),
+        //     Flags::AREA,
+        // )?;
+
+
+        // let mut scaler = Context::get(
+        //     decoder.format(),
+        //     decoder.width(),
+        //     decoder.height(),
+        //     Pixel::GRAY8,
+        //     decoder.width(),
+        //     decoder.height(),
+        //     Flags::AREA,
+        // )?;
 
         let mut frame_index = 0;
 
@@ -58,31 +69,74 @@ fn main() -> Result<(), ffmpeg::Error> {
                 let mut decoded = Video::empty();
                 while decoder.receive_frame(&mut decoded).is_ok() {
                     let mut frame = Video::empty();
+
+
+                    let terminal_size = get_terminal_size();
+
+                    let mut scaler = Context::get(
+                        decoder.format(),
+                        decoder.width(),
+                        decoder.height(),
+                        Pixel::GRAY8,
+                        terminal_size.0,
+                        terminal_size.1,
+                        Flags::AREA,
+                    )?;
+
                     scaler.run(&decoded, &mut frame)?;
+
+                    // println!("{:?} {:?}  {} {:?}",terminal_size.0*terminal_size.1*2,terminal_size,frame.data(0).len(),frame.stride(0));
+            
+            
+                    // let mut scaler = Context::get(
+                    //     decoder.format(),
+                    //     decoder.width(),
+                    //     decoder.height(),
+                    //     Pixel::GRAY8,
+                    //     decoder.width(),
+                    //     decoder.height(),
+                    //     Flags::AREA,
+                    // )?;
+
+                    let mut chars_vec:Vec<char>= Vec::with_capacity((terminal_size.0*terminal_size.1) as usize);
+
+                    let frame_data = frame.data(0);
+
+
+                    for i in 0..terminal_size.1{
+                        for j in 0..terminal_size.0{
+                            let index = i as usize * frame.stride(0) + j as usize;
+                            let c= chars[frame_data[index] as usize / (260 / (chars.len()))];
+                            chars_vec.push(c);
+                        }
+                    }
+
+
+
+                    
 
                     // save_file(downsampled_image,terminal_size.0 as u32,terminal_size.1 as u32,frame_index).unwrap();
 
-                    let terminal_size = terminal::size().unwrap();
 
-                    let downsampled_image = area_downsample(
-                        frame.data(0),
-                        frame.width(),
-                        frame.height(),
-                        terminal_size.0 as u32,
-                        terminal_size.1 as u32,
-                    );
+                    // let downsampled_image = area_downsample(
+                    //     frame.data(0),
+                    //     frame.width(),
+                    //     frame.height(),
+                    //     terminal_size.0 as u32,
+                    //     terminal_size.1 as u32,
+                    // );
 
-                    let chars_vec: String = downsampled_image
-                        .iter()
-                        .map(|&value| chars[value as usize / (260 / (chars.len()))])
-                        .collect();
+                    // let chars_vec: String = downsampled_image
+                    //     .iter()
+                    //     .map(|&value| chars[value as usize / (260 / (chars.len()))])
+                    //     .collect();
 
                     stdout
                         .queue(terminal::Clear(terminal::ClearType::All))
                         .unwrap()
                         .queue(cursor::MoveTo(0, 0))
                         .unwrap()
-                        .queue(Print(chars_vec))
+                        .queue(Print(chars_vec.iter().collect::<String>()))
                         .unwrap()
                         .flush()
                         .unwrap();
@@ -128,56 +182,6 @@ fn main() -> Result<(), ffmpeg::Error> {
 //     Ok(())
 // }
 
-fn area_downsample(
-    input: &[u8],
-    in_width: u32,
-    in_height: u32,
-    out_width: u32,
-    out_height: u32,
-) -> Vec<u8> {
-    // Check if output dimensions are valid
-    if out_width > in_width || out_height > in_height {
-        panic!("Output dimensions cannot be greater than input dimensions.");
-    }
-
-    // Calculate downsample ratios
-    let width_ratio = in_width as f64 / out_width as f64;
-    let height_ratio = in_height as f64 / out_height as f64;
-
-    // Allocate memory for output image
-    let mut output_img = Vec::with_capacity((out_width * out_height) as usize);
-    unsafe {
-        output_img.set_len((out_width * out_height) as usize);
-    }
-
-    for y in 0..out_height {
-        for x in 0..out_width {
-            // Calculate area covered by the output pixel in the input image
-            let in_y_start = (y as f64 * height_ratio).floor() as u32;
-            let in_y_end =
-                f64::min((y as f64 + 1.0) * height_ratio, in_height as f64).floor() as u32;
-            let in_x_start = (x as f64 * width_ratio).floor() as u32;
-            let in_x_end = f64::min((x as f64 + 1.0) * width_ratio, in_width as f64).floor() as u32;
-
-            // Accumulate intensity values within the area
-            let mut intensity_sum: u32 = 0;
-            for in_y in in_y_start..in_y_end {
-                for in_x in in_x_start..in_x_end {
-                    intensity_sum +=
-                        input[in_y as usize * in_width as usize + in_x as usize] as u32;
-                }
-            }
-
-            // Average intensity for the output pixel
-            let area = (in_y_end - in_y_start) * (in_x_end - in_x_start);
-            output_img[y as usize * out_width as usize + x as usize] =
-                ((intensity_sum as u32 + area / 2) / area) as u8;
-        }
-    }
-
-    output_img
-}
-
 
 fn spawn_and_play_audio(path:String){
     thread::spawn(|| {
@@ -193,4 +197,9 @@ fn spawn_and_play_audio(path:String){
         sink.append(source);
         sink.sleep_until_end();
     });
+}
+
+fn get_terminal_size() -> (u32, u32) {
+    let (width, height) = terminal::size().unwrap();
+    (width as u32, height as u32)
 }
